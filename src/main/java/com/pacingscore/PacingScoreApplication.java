29d4eb4ddb405d34a1c08b40dc5212d7c135bce0
DomiCodeur <mathieu.domichard@gmail.com>
2026-02-26 15:14:30 +0100

    feat: Add Spring Boot backend with FFmpeg analyzer

---
+++ src/main/java/com/pacingscore/PacingScoreApplication.java
@@ -0,0 +1,11 @@
+package com.pacingscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacingScoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PacingScoreApplication.class, args);
+    }
+}
